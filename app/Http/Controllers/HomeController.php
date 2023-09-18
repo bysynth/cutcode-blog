93206d90a15ff6199397040efe109bc85294cf1d
VK <belsynth@gmail.com>
2023-09-18 15:29:30 +0300

    feat: добавляет вывод последних 6 статей на главную

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,14 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Models\Article;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\View\View;
 
 class HomeController extends Controller
 {
     public function __invoke(): View
     {
+        $articles = Cache::remember('lastArticles', now()->addHours(12), function () {
+            return Article::latest()->take(6)->get();
+        });
+
         return view('home', [
-            'articles' => Article::take(4)->get(),
+            'articles' => $articles,
         ]);
     }
 }
