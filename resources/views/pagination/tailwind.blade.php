25adcb45aadf50501ac937c66fcc5177b36b18c4
VK <belsynth@gmail.com>
2023-09-20 11:23:59 +0300

    fix: исправляет вью для пагинатора статей

--- resources/views/pagination/tailwind.blade.php
+++ resources/views/pagination/tailwind.blade.php
@@ -1,25 +1,18 @@
 @if ($paginator->hasPages())
     <nav class="mt-4">
         <ul class="flex flex-wrap items-center justify-center gap-3">
-            @foreach ($elements as $element)
-                @if (is_string($element))
-                    <li class="block p-3 text-sm font-black leading-none text-white">{{ $element }}</li>
-                @endif
-                @if (is_array($element))
-                    @foreach ($element as $page => $url)
-                        @if ($page == $paginator->currentPage())
-                            <li class="active block p-3 text-sm font-black leading-none text-pink">
-                                {{ $page }}
-                            </li>
-                        @else
-                            <li>
-                                <a href="{{ $url }}"
-                                   class="block p-3 text-sm font-black leading-none text-white hover:text-pink">
-                                    {{ $page }}
-                                </a>
-                            </li>
-                        @endif
-                    @endforeach
+            @foreach($paginator->getUrlRange(1, $paginator->lastPage()) as $page => $link)
+                @if($page === $paginator->currentPage())
+                    <li class="active block p-3 text-sm font-black leading-none text-pink">
+                        {{ $page }}
+                    </li>
+                @else
+                    <li>
+                        <a href="{{ $link }}"
+                           class="block p-3 text-sm font-black leading-none text-white hover:text-pink">
+                            {{ $page }}
+                        </a>
+                    </li>
                 @endif
             @endforeach
         </ul>
